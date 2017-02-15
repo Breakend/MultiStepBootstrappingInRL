2737846372e4d4d5cda81e95717f2ebd4586416c
Peter Henderson <breakend@wpa041022.wireless.mcgill.ca>
2017-02-15 14:49:13 -0500

    Add deterministic gridworld with n-step sarsas in addition to previous graphs

--- varying_alpha_epsilon_experiment_deterministic_gridworld.py
+++ varying_alpha_epsilon_experiment_deterministic_gridworld.py
@@ -1,57 +1,23 @@
 import numpy as np
 from mdp_matrix import GridWorld, StochasticGridWorld, WindyGridCliffMazeWorld
 from double_sarsa import double_sarsa
+from n_step_sarsa import n_step_sarsa
+from n_step_expected_sarsa import n_step_expected_sarsa
 from expected_sarsa import expected_sarsa
 from double_expected_sarsa import double_expected_sarsa
 import matplotlib.pyplot as plt
 from sarsa import sarsa
 
+start_state = [0, 0]
 
-# TODO: Change this to deterministic gridworld
-
-test_rewards = [[i, j, -1.0] for i in range(10) for j in range(10)]
-test_rewards[59] = [5, 9, 50]
-
-terminal_states = [59]
-
-obstacles = [[i, j, 0] for i in range(10) for j in range(10)]
-obstacles[0*10+3] = [0, 3, 1]
-obstacles[0*10+8] = [0, 8, 1]
-obstacles[1*10+1] = [1, 1, 1]
-obstacles[1*10+4] = [1, 4, 1]
-obstacles[1*10+5] = [1, 5, 1]
-obstacles[1*10+6] = [1, 6, 1]
-obstacles[2*10+1] = [2, 1, 1]
-obstacles[2*10+8] = [2, 8, 1]
-obstacles[3*10+1] = [3, 1, 1]
-obstacles[3*10+4] = [3, 4, 1]
-obstacles[3*10+5] = [3, 5, 1]
-obstacles[3*10+6] = [3, 6, 1]
-obstacles[5*10+0] = [5, 0, 1]
-obstacles[5*10+5] = [5, 5, 1]
-obstacles[6*10+3] = [6, 3, 1]
-obstacles[6*10+4] = [6, 4, 1]
-obstacles[6*10+5] = [6, 5, 1]
-obstacles[6*10+6] = [6, 6, 1]
-obstacles[6*10+8] = [6, 8, 1]
-obstacles[7*10+8] = [7, 8, 1]
-obstacles[9*10+4] = [9, 4, 1]
-
-traps = [0]*100
-traps[4] = 1
-traps[9] = 1
-traps[13] = 1
-traps[33] = 1
-traps[43] = 1
-traps[47] = 1
-traps[67] = 1
-traps[72] = 1
-traps[96] = 1
+test_rewards = [[i, j, -1] for i in range(10) for j in range(10)]
+test_rewards[2] = [0, 2, 1]
+test_rewards[23] = [4,3, 1]
 
 start_state = [0, 0]
 
-gw = WindyGridCliffMazeWorld(10, test_rewards, terminal_states, traps, start_state, obstacles)
-print test_rewards
+gw = GridWorld(10, test_rewards, terminal_states=[2, 23] )
+# print test_rewards
 
 average_reward_double_sarsa = []
 all_rewards_per_episode_double_sarsa = []
@@ -69,9 +35,21 @@ average_reward_sarsa = []
 all_rewards_per_episode_sarsa = []
 q_var_sarsa = []
 
+
+average_reward_n_step_sarsa = []
+all_rewards_per_episode_n_step_sarsa = []
+q_var_n_step_sarsa = []
+
+
+average_reward_n_step_expected_sarsa = []
+all_rewards_per_episode_n_step_expected_sarsa = []
+q_var_n_step_expected_sarsa = []
+
+
+
 epsilon = .1
 
-n=1000
+n=10000
 alphas = [x for x in np.arange(0.0, 1., .05)]
 alphas[0] = .01
 # import pdb; pdb.set_trace()
@@ -97,24 +75,31 @@ for r in range(number_of_runs):
         q_var_sarsa.append(Q_variances)
         average_reward_sarsa.append(average_reward)
         all_rewards_per_episode_sarsa.append(all_rewards)
+        Q, average_reward, max_reward, all_rewards, Q_variances = n_step_sarsa(gw, n, epsilon=epsilon, alpha=alpha)
+        q_var_n_step_sarsa.append(Q_variances)
+        average_reward_n_step_sarsa.append(average_reward)
+        all_rewards_per_episode_n_step_sarsa.append(all_rewards)
+        Q, average_reward, max_reward, all_rewards, Q_variances = n_step_expected_sarsa(gw, n, epsilon=epsilon, alpha=alpha)
+        q_var_n_step_expected_sarsa.append(Q_variances)
+        average_reward_n_step_expected_sarsa.append(average_reward)
+        all_rewards_per_episode_n_step_expected_sarsa.append(all_rewards)
 
 
-# import pdb; pdb.set_trace()
 
 q_var_sarsa = np.mean(np.mean(np.split(np.array(q_var_sarsa), number_of_runs), axis = 0), axis=1)
 q_var_expected_sarsa = np.mean(np.mean(np.split(np.array(q_var_expected_sarsa), number_of_runs), axis = 0), axis=1)
 q_var_double_expected_sarsa = np.mean(np.mean(np.split(np.array(q_var_double_expected_sarsa), number_of_runs), axis = 0), axis=1)
 q_var_double_sarsa = np.mean(np.mean(np.split(np.array(q_var_double_sarsa), number_of_runs), axis = 0), axis=1)
-#
-# print("SARSA Mean Q Variance: %f" % np.mean(q_var_sarsa))
-# print("Expected SARSA Mean Q Variance: %f" % np.mean(q_var_expected_sarsa))
-# print("Double SARSA Mean Q Variance: %f" % np.mean(q_var_double_sarsa))
-# print("Double Expected SARSA Mean Q Variance: %f" % np.mean(q_var_double_expected_sarsa))
+q_var_n_step_sarsa = np.mean(np.mean(np.split(np.array(q_var_n_step_sarsa), number_of_runs), axis = 0), axis=1)
+q_var_n_step_expected_sarsa = np.mean(np.mean(np.split(np.array(q_var_n_step_expected_sarsa), number_of_runs), axis = 0), axis=1)
+
 
 plt.plot(alphas, q_var_double_sarsa, label="Double Sarsa")
 plt.plot(alphas, q_var_expected_sarsa, label="Expected Sarsa")
 plt.plot(alphas, q_var_double_expected_sarsa, label="Double Expected Sarsa")
 plt.plot(alphas, q_var_sarsa, label="Sarsa")
+plt.plot(alphas, q_var_n_step_sarsa, label="N-Step Sarsa")
+plt.plot(alphas, q_var_n_step_expected_sarsa, label="N-Step Expected Sarsa")
 
 plt.ylabel('Average Q Variance')
 plt.xlabel('alpha')
@@ -130,10 +115,15 @@ average_reward_double_sarsa = np.mean(np.split(np.array(average_reward_double_sa
 average_reward_expected_sarsa = np.mean(np.split(np.array(average_reward_expected_sarsa), number_of_runs), axis=0)
 average_reward_double_expected_sarsa = np.mean(np.split(np.array(average_reward_double_expected_sarsa), number_of_runs), axis=0)
 average_reward_sarsa = np.mean(np.split(np.array(average_reward_sarsa), number_of_runs), axis=0)
+average_reward_n_step_sarsa = np.mean(np.split(np.array(average_reward_n_step_sarsa), number_of_runs), axis=0)
+average_reward_n_step_expected_sarsa = np.mean(np.split(np.array(average_reward_n_step_expected_sarsa), number_of_runs), axis=0)
+
 plt.plot(alphas, average_reward_double_sarsa, label="Double Sarsa")
 plt.plot(alphas, average_reward_expected_sarsa, label="Expected Sarsa")
 plt.plot(alphas, average_reward_double_expected_sarsa, label="Double Expected Sarsa")
 plt.plot(alphas, average_reward_sarsa, label="Sarsa")
+plt.plot(alphas, average_reward_n_step_sarsa, label="N-Step Sarsa")
+plt.plot(alphas, average_reward_n_step_expected_sarsa, label="N-Step Exepected Sarsa")
 
 plt.ylabel('Average reward')
 plt.xlabel('alpha')
@@ -148,22 +138,31 @@ print("Max alpha SARSA: %f" % alphas[np.argmax(average_reward_sarsa)])
 print("Max alpha Expected SARSA: %f" % alphas[np.argmax(average_reward_expected_sarsa)])
 print("Max alpha Double SARSA: %f" % alphas[np.argmax(average_reward_double_sarsa)])
 print("Max alpha Double Expected SARSA: %f" % alphas[np.argmax(average_reward_double_expected_sarsa)])
+print("Max alpha N-step SARSA: %f" % alphas[np.argmax(average_reward_n_step_sarsa)])
+print("Max alpha N-step Expected SARSA: %f" % alphas[np.argmax(average_reward_n_step_expected_sarsa)])
 
 
 all_rewards_per_episode_double_sarsa = np.mean(np.split(np.array(all_rewards_per_episode_double_sarsa), number_of_runs), axis=0)
 all_rewards_per_episode_expected_sarsa = np.mean(np.split(np.array(all_rewards_per_episode_expected_sarsa), number_of_runs), axis=0)
 all_rewards_per_episode_double_expected_sarsa = np.mean(np.split(np.array(all_rewards_per_episode_double_expected_sarsa), number_of_runs), axis=0)
 all_rewards_per_episode_sarsa = np.mean(np.split(np.array(all_rewards_per_episode_sarsa), number_of_runs), axis=0)
+all_rewards_per_episode_n_step_sarsa = np.mean(np.split(np.array(all_rewards_per_episode_n_step_sarsa), number_of_runs), axis=0)
+all_rewards_per_episode_n_step_expected_sarsa = np.mean(np.split(np.array(all_rewards_per_episode_n_step_expected_sarsa), number_of_runs), axis=0)
 
 variance_double_sarsa = np.var(all_rewards_per_episode_double_sarsa, axis=1)
 variance_double_expected_sarsa = np.var(all_rewards_per_episode_double_expected_sarsa, axis=1)
 variance_expected_sarsa = np.var(all_rewards_per_episode_expected_sarsa, axis=1)
 variance_sarsa = np.var(all_rewards_per_episode_sarsa, axis=1)
+variance_n_step_sarsa = np.var(all_rewards_per_episode_n_step_sarsa, axis=1)
+variance_n_step_expected_sarsa = np.var(all_rewards_per_episode_n_step_expected_sarsa, axis=1)
+
 # import pdb; pdb.set_trace()/
 plt.plot(alphas, variance_double_sarsa, label="Double Sarsa")
 plt.plot(alphas, variance_expected_sarsa, label="Expected Sarsa")
 plt.plot(alphas, variance_double_expected_sarsa, label="Double Expected Sarsa")
 plt.plot(alphas, variance_sarsa, label="Sarsa")
+plt.plot(alphas, variance_n_step_sarsa, label="N-Step Sarsa")
+plt.plot(alphas, variance_n_step_expected_sarsa, label="N-Step Expected Sarsa")
 
 plt.ylabel('Variance in Reward')
 plt.xlabel('alpha')

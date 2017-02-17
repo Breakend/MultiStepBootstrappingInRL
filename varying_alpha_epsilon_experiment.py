02f276ed802f602a78a509e3546dee0832934dac
Peter Henderson <breakend@wpa041250.wireless.mcgill.ca>
2017-02-17 00:29:58 -0500

    make windy gridworld smaller

--- varying_alpha_epsilon_experiment.py
+++ varying_alpha_epsilon_experiment.py
@@ -13,48 +13,27 @@ import matplotlib.pyplot as plt
 from sarsa import sarsa
 from joblib import Parallel, delayed
 
-test_rewards = [[i, j, -1.0] for i in range(10) for j in range(10)]
-test_rewards[59] = [5, 9, 50]
+test_rewards = [[i, j, -1.0] for i in range(5) for j in range(5)]
+test_rewards[24] = [4, 4, 50]
 
-terminal_states = [59]
+terminal_states = [24]
 
-obstacles = [[i, j, 0] for i in range(10) for j in range(10)]
-obstacles[0*10+3] = [0, 3, 1]
-obstacles[0*10+8] = [0, 8, 1]
-obstacles[1*10+1] = [1, 1, 1]
-obstacles[1*10+4] = [1, 4, 1]
-obstacles[1*10+5] = [1, 5, 1]
-obstacles[1*10+6] = [1, 6, 1]
-obstacles[2*10+1] = [2, 1, 1]
-obstacles[2*10+8] = [2, 8, 1]
-obstacles[3*10+1] = [3, 1, 1]
-obstacles[3*10+4] = [3, 4, 1]
-obstacles[3*10+5] = [3, 5, 1]
-obstacles[3*10+6] = [3, 6, 1]
-obstacles[5*10+0] = [5, 0, 1]
-obstacles[5*10+5] = [5, 5, 1]
-obstacles[6*10+3] = [6, 3, 1]
-obstacles[6*10+4] = [6, 4, 1]
-obstacles[6*10+5] = [6, 5, 1]
-obstacles[6*10+6] = [6, 6, 1]
-obstacles[6*10+8] = [6, 8, 1]
-obstacles[7*10+8] = [7, 8, 1]
-obstacles[9*10+4] = [9, 4, 1]
+obstacles = [[i, j, 0] for i in range(5) for j in range(5)]
+
+
+obstacles[1*5+1] = [1, 1, 1]
+obstacles[2*5+2] = [2, 2, 1]
+obstacles[2*5+3] = [2, 3, 1]
+obstacles[3*5+0] = [3, 0, 1]
+obstacles[4*5+3] = [4, 3, 1]
 
 traps = [0]*100
-traps[4] = 1
-traps[9] = 1
-traps[13] = 1
-traps[33] = 1
-traps[43] = 1
-traps[47] = 1
-traps[67] = 1
-traps[72] = 1
-traps[96] = 1
+traps[1*5+2] = 1
+traps[1*5+3] = 1
 
 start_state = [0, 0]
 
-gw = WindyGridCliffMazeWorld(10, test_rewards, terminal_states, traps, start_state, obstacles)
+gw = WindyGridCliffMazeWorld(5, test_rewards, terminal_states, traps, start_state, obstacles)
 # print test_rewards
 
 average_reward_double_sarsa = []
